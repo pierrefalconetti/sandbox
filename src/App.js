63f0469372d48f44f226cd7f0c4337a3f9de4020
Benjamin Dean <benjamin.dean@sage.com>
2022-03-16 12:00:46 +0100

    This is my message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>Hello World!</div>
+
+        <Popup trigger={<button>Lorem Ipsum is here</button>} position="right center">
+          <div>Popup content here!!</div>
+        </Popup>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
