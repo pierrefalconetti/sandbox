580894b7adc37e120b55be07fa0ec0c06de80839
pierrefalconetti <pierre.falconetti@sage.com>
2022-07-20 16:21:44 +0200

    Reshaping cards on the columns-grid system.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,24 +10,26 @@ export interface CardProps {
 function Card(props: CardProps) {
   return (
     <div className="card">
-      <div className="card-header">{props.content}</div>
-      <div className="card-subheader">
-      { props.email ? (
-          // If the e-mail address is provided, display this template:
-          <div className="email">
-            E-mail:&nbsp;
-            <a href={`mailto: ${props.email}`}>
-              {props.email}
-            </a>
-          </div>
-        ) : (
-          // If there is no e-mail address, use this other template:
-          <></>
-        )
-      }
-      </div>
-      <div className="card-content">
-        {props.children}
+      <div className="card-container">
+        <div className="card-header">{props.content}</div>
+        <div className="card-subheader">
+        { props.email ? (
+            // If the e-mail address is provided, display this template:
+            <div className="email">
+              E-mail:&nbsp;
+              <a href={`mailto: ${props.email}`}>
+                {props.email}
+              </a>
+            </div>
+          ) : (
+            // If there is no e-mail address, use this other template:
+            <></>
+          )
+        }
+        </div>
+        <div className="card-content">
+          {props.children}
+        </div>
       </div>
     </div>
   )
