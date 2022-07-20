bada8351512ef65f972c1d6c3837b03a5b30d608
pierrefalconetti <pierre.falconetti@sage.com>
2022-07-20 11:46:00 +0200

    Adding CSS-SASS capacity to the project - I did forget to change the references of the stylesheets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import React from 'react';
 import MyNewComponent from './xtrem-header';
 import Popup from 'reactjs-popup';
