0d3ddc0eae040c09ebff61aa5fdd52aec3ba6fce
Benjamin Dean <benjamin_h_dean@yahoo.com>
2022-10-07 18:36:11 +0200

    Update WidgetValue.tsx
    children with any

--- src/components/WidgetValue/WidgetValue.tsx
+++ src/components/WidgetValue/WidgetValue.tsx
@@ -3,15 +3,16 @@ import './WidgetValue.scss';
 
 
 export interface Template {
+  children?: any;
 
-  }
-  
-  function WidgetValue(props: Template) {
-    return (
-      <div className="WidgetTemplate">
-        Hello World!
-      </div>
-    )
-  }
+}
 
-  export default WidgetValue;
+function WidgetValue(props: Template) {
+  return (
+    <div className="WidgetTemplate">
+      Hello World!
+    </div>
+  )
+}
+
+export default WidgetValue;
